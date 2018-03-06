e34ae099c16c8ea36fa0012962a6ce29c7b9e36f
Rummel17 <p.rummel17@gmail.com>
2018-03-06 22:32:49 +0100

    final1.7

---
+++ resources/js/cookie.js
@@ -0,0 +1,14 @@
+<script>
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#252e39"
+    },
+    "button": {
+      "background": "#14a7d0"
+    }
+  },
+  "theme": "classic"
+})});
+</script>
