a625a3d2e4b9441b7d1280d54796531759b67c80
Rummel17 <p.rummel17@gmail.com>
2018-03-13 07:41:40 +0100

    b

---
+++ resources/js/cookie.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "rgba(6,39,59,1)"
+    },
+    "button": {
+      "background": "transparent",
+      "text": "#14a7d0",
+      "border": "#14a7d0"
+    }
+  }
+})});
